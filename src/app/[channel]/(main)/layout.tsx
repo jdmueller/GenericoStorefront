e43fff3445a4b2ced525ea15065830aa634cc475
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-18 17:13:17 -0500

    page titles and descriptions

--- src/app/[channel]/(main)/layout.tsx
+++ src/app/[channel]/(main)/layout.tsx
@@ -3,8 +3,8 @@ import { Footer } from "@/ui/components/Footer";
 import { Header } from "@/ui/components/Header";
 
 export const metadata = {
-	title: "Saleor Storefront example",
-	description: "Starter pack for building performant e-commerce experiences with Saleor.",
+	title: "Genérico Ecuador, CBD Products For Your Health",
+	description: "CBD Products | CBD Oil, CBD Gummies, CBD Beer, CBD Coffee, CBD Creams.",
 };
 
 export default function RootLayout(props: { children: ReactNode; params: { channel: string } }) {
--- src/app/[channel]/(main)/page.tsx
+++ src/app/[channel]/(main)/page.tsx
@@ -3,9 +3,8 @@ import { executeGraphQL } from "@/lib/graphql";
 import { ProductList } from "@/ui/components/ProductList";
 
 export const metadata = {
-	title: "ACME Storefront, powered by Saleor & Next.js",
-	description:
-		"Storefront Next.js Example for building performant e-commerce experiences with Saleor - the composable, headless commerce platform for global brands.",
+	title: "Genérico Ecuador, CBD Products For Your Health",
+	description: "CBD Products | CBD Oil, CBD Gummies, CBD Beer, CBD Coffee, CBD Creams.",
 };
 
 export default async function Page({ params }: { params: { channel: string } }) {
