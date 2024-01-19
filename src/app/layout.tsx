a1cd5cdb6601250885b0949d1aa4537df1c0de34
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-19 08:56:56 -0500

    loading channel default, page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { DraftModeNotification } from "@/ui/components/DraftModeNotification";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Saleor Storefront example",
-	description: "Starter pack for building performant e-commerce experiences with Saleor.",
+	title: "Genérico Ecuador, CBD Products For Your Health",
+	description: "CBD Products | CBD Oil, CBD Gummies, CBD Beer, CBD Coffee, CBD Creams.",
 	metadataBase: process.env.NEXT_PUBLIC_STOREFRONT_URL
 		? new URL(process.env.NEXT_PUBLIC_STOREFRONT_URL)
 		: undefined,
