5f675db9c62bbecc46d75292275d6f56bd7bb383
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-22 12:38:21 -0500

    revert pixel padding on sm

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -4,7 +4,7 @@ import { Nav } from "./nav/Nav";
 export function Header({ channel }: { channel: string }) {
 	return (
 		<header className="sticky top-0 z-20 bg-green-500/80 backdrop-blur-md">
-			<div className="mx-auto max-w-7xl px-3 ">
+			<div className="mx-auto max-w-7xl px-3 sm:px-8">
 				<div className="flex h-24 justify-between gap-4 md:gap-8">
 					<Logo />
 					<Nav channel={channel} />
