83c36b9d79a7c4764e689ca182bb686b504950b3
Jonathan Mueller <jdmueller@protonmail.ch>
2024-01-22 11:30:15 -0500

    checkout logo padding top 2

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -21,7 +21,7 @@ export default function CheckoutPage({
 
 	return (
 		<div className="min-h-dvh bg-white">
-			<section className="mx-auto flex min-h-dvh max-w-7xl flex-col p-8">
+			<section className="p2 mx-auto flex min-h-dvh max-w-7xl flex-col">
 				<div className="flex items-center font-bold">
 					<a aria-label="homepage" href="/">
 						<Image alt={companyName} height={106} width={130} src={"/Generico-EC.svg"} />
